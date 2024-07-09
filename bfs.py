34fd868ac66c057e325807fcd898e885f3b40c93
Saroj Maharjan <maharjan.saroj93@gmail.com>
2024-07-10 05:44:07 +0545

    bfs with goal node

--- bfs.py
+++ bfs.py
@@ -10,20 +10,25 @@ graph = {
     'I': []
 }
 
-visited = set()  # set for visited nodes so that no any nodes are repeated.
-queue = []  # Initialize a queue
+def bfs(graph, node, goal_node):  # function for BFS
+   
+    visited = set()  # set for visited nodes so that no any nodes are repeated.
+    queue = []  # Initialize a queue
 
+    if node == goal_node:
+        return node
 
-def bfs(graph, node):  # function for BFS
     visited.add(node)
     queue.append(node)
 
     while queue:          # Creating loop to visit each node
-        m = queue.pop(0)
+        path = queue.pop(0)
         # print '->' after each node except the last one
-        print(m, end='->' if m != 'I' else '')
+        print(path, end='->' if path != f'{goal_node}' else '')
+        if path == goal_node:
+            return path
 
-        for neighbour in graph[m]:
+        for neighbour in graph[path]:
             if neighbour not in visited:
                 visited.add(neighbour)
                 queue.append(neighbour)
@@ -31,4 +36,4 @@ def bfs(graph, node):  # function for BFS
 
 # Driver Code
 print("Following is the Breadth-First Search")
-bfs(graph, 'A')
+bfs(graph, 'A','K')
