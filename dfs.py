6f612cdc237a813cdf5d11b215e9c8aebd64bcd7
Saroj Maharjan <maharjan.saroj93@gmail.com>
2024-07-11 07:24:59 +0545

    dfs using Recursion

---
+++ dfs.py
@@ -0,0 +1,22 @@
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:        
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+
+# Using a Python dictionary to act as an adjacency list
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
